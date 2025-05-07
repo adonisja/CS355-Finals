c980b3a79c18d724949a3035e1a41c5ba399741d
ndhilliard <n_hilliard@hotmail.com>
2025-05-07 16:32:29 -0400

    Add files via upload

---
+++ screens/MainScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function MainScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Put my Queries here when i Make them...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+  },
+  text: {
+    fontSize: 20,
+    color: '#333',
+  },
+});
---
+++ screens/welcomeScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function WelcomeScreen() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <Image 
+                source={require('../assets/image/welcome.jpg')}
+                style={styles.bg}
+                resizeMode="cover"
+            />
+            <Text style={styles.title}>Welcome to IMBD Director Query App</Text>
+            <Text style={styles.subtitle}>By Nathan Hilliard and Akkeem Tyrell</Text>
+
+            <TouchableOpacity 
+                style={styles.button}
+                onPress={() => navigation.navigate('Main')}
+            >
+                <Text style={styles.buttonText}>Press to Start!</Text>
+            </TouchableOpacity>
+            </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    image: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontSize: 16,
+        color: '#666',
+        marginBottom: 40,
+    },
+    button: {
+        backgroundColor: '#E50914',
+        paddingVertical: 12,
+        paddingHorizontal: 30,
+        borderRadius: 25,
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
