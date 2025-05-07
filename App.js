3a6c5e7726a3684e5260d3b01ab24724a794cbde
ndhilliard <n_hilliard@hotmail.com>
2025-05-07 16:32:29 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import WelcomeScreen from './screens/welcomeScreen';
+import MainScreen from './screens/MainScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name ="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name ="Main" component={MainScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
